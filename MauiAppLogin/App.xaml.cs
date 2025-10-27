1966e351ecc573f07379a89ede2b4438fffcec57
juare-moura <juarez.santos15@etec.sp.gov.br>
2025-10-26 22:53:24 -0300

    Add  arquivos ao projeto

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace MauiAppLogin
+﻿
+namespace MauiAppLogin
 {
     public partial class App : Application
     {
@@ -6,7 +7,14 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new Login();
         }
-    }
-}
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Width = 400;
+            window.Height = 600;
+            return window;
+        }
+    }// Fecha Classe
+}// Fecha namespace
---
+++ MauiAppLogin/Login.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiAppLogin;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
